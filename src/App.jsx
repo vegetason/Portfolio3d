ea408b88dca096be9396a8dce0ab28632f83ca1e
vegetason <irakozepaulin12@gmail.com>
2025-09-13 09:44:01 +0200

    Setup Project, Create Navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from './sections/NavBar'
+
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <NavBar/>
+    </main>
+  )
+}
+
+export default App
