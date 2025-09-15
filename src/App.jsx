8d288f15d2d2440c1c7cb364c97c3db3abab8081
vegetason <irakozepaulin12@gmail.com>
2025-09-15 20:08:58 +0200

    Finish Projects Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './sections/NavBar'
 import Hero from './sections/Hero'
 import About from './sections/About'
+import Projects from './sections/Projects'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NavBar/>
       <Hero/>
       <About/>
+      <Projects/>
     </main>
   )
 }
